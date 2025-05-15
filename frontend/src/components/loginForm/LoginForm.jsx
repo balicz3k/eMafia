22a19da68f39b4d08847e53ce23cf8786007637e
balicz3k <baliczek_10@vp.pl>
2025-05-15 06:59:26 +0200

    feat: Update LoginForm component with enhanced styling and functionality, including username input and remember me feature

--- frontend/src/components/loginForm/LoginForm.jsx
+++ frontend/src/components/loginForm/LoginForm.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import styles from './LoginForm.module.css';
+import { MdEmail, MdLock, MdPerson } from 'react-icons/md';
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const LoginForm = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
+  const [remember, setRemember] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/dashboard");
+    }
+  }, [navigate]);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
+  const handleRememberChange = () => {
+    setRemember(!remember);
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -24,7 +36,6 @@ const LoginForm = () => {
       if (response.ok) {
         const data = await response.json();
         localStorage.setItem("token", data.token);
-        console.log("Token JWT:", data.token);
         navigate("/dashboard");
       } else {
         const error = await response.text();
@@ -37,27 +48,83 @@ const LoginForm = () => {
   };
 
   return (
-    <form className={styles.loginForm} onSubmit={handleSubmit}>
-      <h2>Sign in!</h2>
-      <div>
-        <label>Email:</label>
-        <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" name="password" value={formData.password} onChange={handleChange} required />
-      </div>
-      <label>By continuing, you agree to the Terms of use and Privacy Policy.</label>
-      <div className={styles.rememberMe}>
-        <input type="checkbox" name="remember" />
-        <label>Remember me</label>
-      </div>
-      <button type="submit">Login</button>
-      <label>Forget your password?</label>
-      <label>
-        Don't have an account? <Link to="/register">Sign up!</Link>
-      </label>
-    </form>
+    <div className={styles.container}>
+        <div className={styles.signinForm}>
+          <h2 className={styles.formTitle}>Sign in</h2>
+          <form onSubmit={handleSubmit}>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="username">
+                <MdPerson size={25} />
+              </label>
+              <input 
+                type="username" 
+                name="username" 
+                id="username"
+                placeholder="Your username"
+                value={formData.username} 
+                onChange={handleChange} 
+                required 
+              />
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="email">
+                <MdEmail size={25} />
+              </label>
+              <input 
+                type="email" 
+                name="email" 
+                id="email"
+                placeholder="Your Email"
+                value={formData.email} 
+                onChange={handleChange} 
+                required 
+              />
+            </div>
+            
+            <div className={styles.formGroup}>
+              <label htmlFor="password">
+                <MdLock size={25} />
+              </label>
+              <input 
+                type="password" 
+                name="password" 
+                id="password"
+                placeholder="Password" 
+                value={formData.password} 
+                onChange={handleChange} 
+                required 
+              />
+            </div>
+            
+            <div className={styles.cleanFormGroup}>
+              <div className={styles.rememberWrapper}>
+                <input 
+                  type="checkbox" 
+                  name="remember" 
+                  id="remember-me"
+                  className={styles.agreeTerm} 
+                  checked={remember}
+                  onChange={handleRememberChange}
+                />
+                <label htmlFor="remember-me" className={styles.rememberMe}>
+                  Remember me
+                </label>
+              </div>
+              <Link className={styles.forgotPassword} to="/forgot-password">Forgot password?</Link>
+            </div>
+            
+            <button className={styles.loginButton}>
+              Log in
+            </button>
+          </form>
+        
+        </div>
+      <Link to="/register" className={styles.signUpLink}>
+        Create an account
+      </Link>
+    </div>
   );
 };
 
