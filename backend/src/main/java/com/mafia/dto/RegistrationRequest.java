afe32cc1e82ec6175e921ae08ce1608a19cb8f6d
balicz3k <baliczek_10@vp.pl>
2025-05-27 09:56:15 +0200

    feat: Implement UserServiceTest to validate user registration, authentication, and management functionalities, adjustment dto classes constructors

--- backend/src/main/java/com/mafia/dto/RegistrationRequest.java
+++ backend/src/main/java/com/mafia/dto/RegistrationRequest.java
@@ -19,6 +19,15 @@ public class RegistrationRequest
              message = "Password must contain at least one lowercase letter, one uppercase letter, and one digit")
     private String password;
 
+    public RegistrationRequest() {};
+
+    public RegistrationRequest(String username, String email, String password)
+    {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
     public String getUsername() { return username; }
 
     public void setUsername(String username) { this.username = username; }
